f86356fa11c596fa214171cd494bdddef7a38ca8
RonaldoVindas <44733416+RonaldoVindas@users.noreply.github.com>
2020-07-26 07:09:59 -0600

    User Interface Ready To Go!

---
+++ User Interface/LA_BELLE_EPOQUE/src/LA_BELLE_EPOQUE/LA_BELLE_EPOQUE.java
@@ -0,0 +1,20 @@
+
+package LA_BELLE_EPOQUE;
+
+import UIFrames.Login_Menu;
+
+/**
+ *
+ * @author Ronaldo
+ */
+public class LA_BELLE_EPOQUE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Login_Menu menu = new Login_Menu();
+       menu.setVisible(true);
+    }
+    
+}
